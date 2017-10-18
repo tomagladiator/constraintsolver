fc24d4be25a77193b41dd709f064425e4d3b9978
Thomas Desfossez <constraintsolver@gmail.com>
2017-10-18 16:50:18 -0400

    Add title

--- static.config.js
+++ static.config.js
@@ -59,7 +59,8 @@ export default {
           <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css" />            
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.min.css" />  
+            <title>Thomas Desfossez - Lead Front-end Architect</title>
             {styleTags}
           </Head>
           <Body>{newChildren}</Body>
