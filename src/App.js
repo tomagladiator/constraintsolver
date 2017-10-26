6fa2ee2dac4f0e573d9aaf751afed1396d73fdfe
tomagladiator <constrainsolver@gmail.com>
2017-10-26 00:14:29 -0400

    work service

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import { getSiteProps } from 'react-static'
 //
 import Routes from 'react-static-routes'
 
-/*
-*/
+// Check for browser support of service worker
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('js/service-workers.js')
+	.then(function(registration) {
+		// Successful registration
+		console.log('Hooray. Registration successful, scope is:', registration.scope);
+	}).catch(function(err) {
+		// Failed registration, service worker won’t be installed
+		console.log('Whoops. Service worker registration failed, error:', error);
+	});
+}
 
 export class Navbar extends React.Component {
 	constructor(props) {
