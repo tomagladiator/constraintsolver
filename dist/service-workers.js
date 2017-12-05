5771e10ae882dd5d3f0e63cce11daf21ea06311b
tomagladiator <constrainsolver@gmail.com>
2017-12-04 23:33:28 -0500

    add push

--- dist/service-workers.js
+++ dist/service-workers.js
@@ -1,8 +1,13 @@
 // NEED TO STAY IN THE ROOT to control directories : https://codelabs.developers.google.com/codelabs/offline/#5
-
+var randomnumber=Math.random()*5;importScripts('https://cdn.izooto.com/scripts/workers/268a624a409ca0caf8f6af9094e731f700481721.js?'+randomnumber);
 var CACHE_NAME = 'tdesfossez-cache';
 var urlsToCache = [
   '/',
+  '/blog/post/avoir-une-font-parfaitement-fluide-entre-deux-breakpoints-fixes/',
+  '/blog/post/simplifier-au-minimum-le-reset-de-votre-css-avec-foundation/',
+  'https://www.thomas-desfossez.com/blog/post/avoir-une-font-parfaitement-fluide-entre-deux-breakpoints-fixes/',
+  '/realisations',
+  '/blog',
   '/index.html',
   '/react-static-routes.js',
   '/app.js',
