4092debe46a54894ed62ba205e9bc886775bf9e9
tomagladiator <constrainsolver@gmail.com>
2017-12-05 00:12:43 -0500

    add push 2

--- public/service-workers.js
+++ public/service-workers.js
@@ -1,14 +1,13 @@
 // NEED TO STAY IN THE ROOT to control directories : https://codelabs.developers.google.com/codelabs/offline/#5
 var randomnumber=Math.random()*5;importScripts('https://cdn.izooto.com/scripts/workers/268a624a409ca0caf8f6af9094e731f700481721.js?'+randomnumber);
-var CACHE_NAME = 'tdesfossez-cache';
+var CACHE_NAME = 'tdesfossez-cache-2';
 var urlsToCache = [
   '/',
-  '/blog/post/avoir-une-font-parfaitement-fluide-entre-deux-breakpoints-fixes/',
+  '/blog/post/head-comprendre-les-balises-et-les-elements-dans-la-balise-head/',
   '/blog/post/simplifier-au-minimum-le-reset-de-votre-css-avec-foundation/',
   '/blog/post/avoir-une-font-parfaitement-fluide-entre-deux-breakpoints-fixes/',
   '/realisations',
   '/blog',
-  '/index.html',
   '/react-static-routes.js',
   '/app.js',
   '/css/bulma.css'
